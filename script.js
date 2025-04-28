8a1db8f515a3e893bd0f400919db65e2fe25f8da
Vikas-Krishna1 <150981669+Vikas-Krishna1@users.noreply.github.com>
2025-04-28 13:18:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Example: Simple scroll to top button functionality
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("scrollBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
